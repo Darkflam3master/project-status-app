038d860129306c8cbd38ff3f8c538024abb15ed7
CharlieXue <x.charlie42@gmail.com>
2021-08-20 13:16:13 -0400

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import { LeftNavBar } from "./components/LeftNavBar";
 
 function App() {
   return (
     <div className="App">
-      <div className="border-2 border-blue-600">
-        <h1>Hello World</h1>
-      </div>
+      <LeftNavBar></LeftNavBar>
     </div>
   );
 }
