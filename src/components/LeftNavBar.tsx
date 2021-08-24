312f5328949554d6c62b29b5c547bc10c49bdb39
CharlieXue <x.charlie42@gmail.com>
2021-08-24 11:40:44 -0400

    react icon instead

--- src/components/LeftNavBar.tsx
+++ src/components/LeftNavBar.tsx
@@ -8,7 +8,22 @@ export const LeftNavBar = (props: Props) => {
     <Router>
       <nav className="bg-gray-900 w-1/6 h-screen">
         <div className="flex flex-col p-4 text-white">
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z"
+                clip-rule="evenodd"
+              />
+              <path d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+            </svg>
+            All Projects
+          </Link>
           <Link to="/createProject">Create Project</Link>
           <Link to="/admin">Admin</Link>
         </div>
