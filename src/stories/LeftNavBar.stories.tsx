414c19ac4c87b080b97311f80dfd419d0207451c
CharlieXue <x.charlie42@gmail.com>
2021-08-26 11:36:17 -0400

    clean up

---
+++ src/stories/LeftNavBar.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import { LeftNavBar } from "../components/3_Organisms/LeftNavBar";
+
+const meta: Meta = {
+  title: "LeftNavBar",
+  component: LeftNavBar,
+};
+
+export default meta;
+
+const leftNavItems = [
+  { name: "All Projects", route: "/", icon: "OpenFolder" },
+  { name: "Red Projects", route: "/red", icon: "ExclaimTriangle" },
+  { name: "Yellow Projects", route: "/yellow", icon: "ExclaimCircle" },
+  { name: "Green Projects", route: "/green", icon: "ThumbsUp" },
+  { name: "Create Projects", route: "/create", icon: "Plus" },
+  { name: "Admin", route: "/admin", icon: "Lock" },
+];
+
+export const Default = () => <LeftNavBar navItems={leftNavItems}></LeftNavBar>;
